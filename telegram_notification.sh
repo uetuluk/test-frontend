c128d1f3ec3443fe31aa326ba4b3baddac119a4f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-23 09:57:25 +0000

    Deploy uetuluk/test-frontend to github.com/uetuluk/test-frontend.git:gh-pages

---
+++ telegram_notification.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+BOT_URL="https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage"
+
+PARSE_MODE="Markdown"
+
+TRAVIS_COMMIT_NAME=$TRAVIS_COMMIT
+
+if [ $TRAVIS_TEST_RESULT -ne 0 ]; then
+    build_status="\`failed\`"
+else
+    build_status="passed"
+fi
+
+send_msg () {
+    curl -s -X POST ${BOT_URL} -d chat_id=259198083 \
+        -d text="$1" -d parse_mode=${PARSE_MODE} -d disable_web_page_preview=True
+}
+
+send_msg "
+Build [#$TRAVIS_BUILD_NUMBER]($TRAVIS_BUILD_WEB_URL) ([${TRAVIS_COMMIT_NAME:0:7}](https://github.com/$TRAVIS_REPO_SLUG/$TRAVIS_COMMIT)) of $TRAVIS_REPO_SLUG@$TRAVIS_PULL_REQUEST_BRANCH ${build_status}. [CHECK IT OUT](https://uetuluk.github.io/test-frontend/)
+
+[Job Log here](${TRAVIS_JOB_WEB_URL})
+"
